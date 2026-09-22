ec37b7e4c19471f9155c7f5d7628c5e892fc17d2
padilha77 <padilhajoaopauloqq@gmail.com>
2026-09-21 21:31:34 -0300

    criado métodos getters e setters para volume, ligado e tocando

--- src/ControleRemoto.java
+++ src/ControleRemoto.java
@@ -11,4 +11,29 @@ public class ControleRemoto {
         this.ligado = false;
         this.tocando = false;
     }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
+
+    public boolean isLigado() {
+        return ligado;
+    }
+
+    public void setLigado(boolean ligado) {
+        this.ligado = ligado;
+    }
+
+    public boolean isTocando() {
+        return tocando;
+    }
+
+    public void setTocando(boolean tocando) {
+        this.tocando = tocando;
+    }
+
 }
