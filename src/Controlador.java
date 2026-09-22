f4fcd9c9bad017dc932767f78ed0e3bcbf282c29
padilha77 <padilhajoaopauloqq@gmail.com>
2026-09-21 21:23:09 -0300

    Criando um sistema simples de um controle remoto

---
+++ src/Controlador.java
@@ -0,0 +1,14 @@
+interface Controlador {
+
+    public abstract void ligar();
+    public abstract void desligar();
+    public abstract void abrirMenu();
+    public abstract void fecharMenu();
+    public abstract void ligarMudo();
+    public abstract void desligarMudo();
+    public abstract void aumentarVolume();
+    public abstract void abaixarVolume();
+    public abstract void play();
+    public abstract void pause();
+
+}
---
+++ src/ControleRemoto.java
@@ -0,0 +1,14 @@
+public class ControleRemoto {
+    //atributos
+    private int volume;
+    private boolean ligado;
+    private boolean tocando;
+
+    //métodos especiais
+
+    public ControleRemoto(int volume, boolean ligado, boolean tocando) {
+        this.volume = 0;
+        this.ligado = false;
+        this.tocando = false;
+    }
+}
